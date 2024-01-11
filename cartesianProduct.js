e321efcbff74b4e75de364dbcf48a59379ff902b
Findlad <findlad@gmail.com>
2024-01-11 14:08:57 -0700

    working on cartesian

--- cartesianProduct.js
+++ cartesianProduct.js
@@ -8,13 +8,6 @@ let phase2Comb = cartesian(...phase2);
 let phase3Comb = cartesian(...phase3);
 let phase4Comb = cartesian(...phase4);
 
-let allCombinations = cartesian(
-  ...phase1Comb,
-  phase2Comb,
-  phase3Comb,
-  phase4Comb
-);
+let allCombinations = cartesian(phase1Comb, phase2Comb, phase3Comb, phase4Comb);
 
-allCombinations.sort((a, b) => a.totalCost - b.totalCost);
-let topTen = allCombinations.slice(0, 9);
-console.log(topTen);
+allCombinations.array.forEach((element) => {});
